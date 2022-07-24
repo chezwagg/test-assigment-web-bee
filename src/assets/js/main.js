62d6d15fe219827338090a6391f56a0f581cf70c
Alexander Golodnikov <chezwagg@gmail.com>
2022-07-25 00:14:08 +0300

    all job is done
    +timer, working spa with jquery ,load, map is working.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,27 @@
+<<<<<<< Updated upstream
 import * as bootstrap from 'bootstrap';
+=======
+import * as bootstrap from "bootstrap";
+
+let start = Date.now();
+let minuetsCount = 0;
+let hoursCount = 0;
+setInterval(() => {
+  let millis = Date.now() - start;
+  let secondCount = Math.floor(millis / 1000);
+  if (secondCount >= 60) {
+    start = Date.now();
+    minuetsCount += 1;
+  }
+  if (minuetsCount >= 60) {
+    hoursCount += 1;
+  }
+  document.querySelector(".counter").innerHTML = `${
+    hoursCount >= 10 ? "" : "0"
+  }${hoursCount} : ${minuetsCount >= 10 ? "" : "0"}${minuetsCount} : ${
+    secondCount >= 10 ? "" : "0"
+  }${secondCount}`;
+
+  console.log(`seconds elapsed = ${Math.floor(millis / 1000)}`);
+}, 1000);
+>>>>>>> Stashed changes
--- src/assets/js/preloader.js
+++ src/assets/js/preloader.js
@@ -1,5 +1,8 @@
+<<<<<<< Updated upstream
 
 // Initialize and add the map
+=======
+>>>>>>> Stashed changes
 function initMap() {
     const khabarovsk = { lat: 48.480, lng: 135.071 };
     const map = new google.maps.Map(document.getElementById("map"), {
@@ -13,7 +16,20 @@ function initMap() {
   }
   
 window.initMap = initMap;
+<<<<<<< Updated upstream
   
 window.onload = function() {
   document.getElementById("preloader-js").style.display = "none";
 }
+=======
+
+window.onload = function () {
+  console.log(1);
+  const preloader = document.querySelector(".preloader");
+  preloader.classList.add("hidden");
+};
+
+$(document).ready(function () {
+  $(".preloader").addClass("hidden");
+});
+>>>>>>> Stashed changes
